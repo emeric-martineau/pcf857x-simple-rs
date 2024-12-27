12b28c0bcf505c40ba7776d4353634505dbc4b6c
Emeric MARTINEAU <11473190+emeric-martineau@users.noreply.github.com>
2024-12-27 12:43:08 +0100

    Convert to embedded-hal v1

--- src/pcf8574.rs
+++ src/pcf8574.rs
@@ -1,13 +1,13 @@
 //! Mod to use PCF8574
 use crate::{Pin, PinState};
-use embedded_hal::blocking::i2c::{self, Read};
+use embedded_hal::i2c::I2c;
 
 // Yes, I have duplicate the code, cause I find that macro is really hard to read.
 
 /// Struct to manage Pcf8574
 pub struct Pcf8574<'a, I>
 where
-    I: i2c::Write + i2c::Write,
+    I: I2c,
 {
     /// I2C driver
     i2c: &'a mut I,
@@ -19,7 +19,7 @@ where
 
 impl<'a, I> Pcf8574<'a, I>
 where
-    I: i2c::Write + i2c::Read,
+    I: I2c,
 {
     /// Create a new struct to manage Pcf8575
     pub fn new(i2c: &'a mut I, address: u8) -> Self {
@@ -31,21 +31,21 @@ where
     }
 
     /// Turn off all pins
-    pub fn clear(&mut self) -> Result<(), <I as i2c::Write>::Error> {
+    pub fn clear(&mut self) -> Result<(), I::Error> {
         self.i2c.write(self.address, &[0])?;
         self.pins_state = 0;
         Ok(())
     }
 
     /// Turn on all pins
-    pub fn all_on(&mut self) -> Result<(), <I as i2c::Write>::Error> {
+    pub fn all_on(&mut self) -> Result<(), I::Error> {
         self.i2c.write(self.address, &[0xff])?;
         self.pins_state = 0xff;
         Ok(())
     }
 
     /// Send two bytes
-    pub fn write(&mut self, value: u8) -> Result<(), <I as i2c::Write>::Error> {
+    pub fn write(&mut self, value: u8) -> Result<(), I::Error> {
         let r = self.i2c.write(self.address, &[value]);
 
         if r.is_ok() {
@@ -56,7 +56,7 @@ where
     }
 
     /// Set up pins, don't change other
-    pub fn up_pins(&mut self, value: &[Pin]) -> Result<(), <I as i2c::Write>::Error> {
+    pub fn up_pins(&mut self, value: &[Pin]) -> Result<(), I::Error> {
         let mut new_value = self.pins_state;
 
         for pin in value {
@@ -67,7 +67,7 @@ where
     }
 
     /// Set up pins, don't change other
-    pub fn down_pins(&mut self, value: &[Pin]) -> Result<(), <I as i2c::Write>::Error> {
+    pub fn down_pins(&mut self, value: &[Pin]) -> Result<(), I::Error> {
         let mut new_value = self.pins_state;
 
         for pin in value {
@@ -78,7 +78,7 @@ where
     }
 
     /// Invert pins
-    pub fn toogle_pins(&mut self, value: &[Pin]) -> Result<(), <I as i2c::Write>::Error> {
+    pub fn toogle_pins(&mut self, value: &[Pin]) -> Result<(), I::Error> {
         let mut new_value = self.pins_state;
 
         for pin in value {
@@ -95,7 +95,7 @@ where
     }
 
     /// Set up don't change other
-    pub fn set_pins(&mut self, value: &[PinState]) -> Result<(), <I as i2c::Write>::Error> {
+    pub fn set_pins(&mut self, value: &[PinState]) -> Result<(), I::Error> {
         let mut new_value = self.pins_state;
 
         for state in value {
@@ -124,7 +124,7 @@ where
     }
 
     /// Read all pins status. Don't update internal state of this struct
-    pub fn read_pins(&mut self) -> Result<u8, <I as Read<u8>>::Error> {
+    pub fn read_pins(&mut self) -> Result<u8, I::Error> {
         let mut buffer: [u8; 1] = [0];
 
         self.i2c.read(self.address, &mut buffer)?;
@@ -136,7 +136,7 @@ where
 
     /// Read all pins status. Don't update internal state of this struct.
     /// /// Return true if pin is up.
-    pub fn read_pin(&mut self, pin: Pin) -> Result<bool, <I as Read<u8>>::Error> {
+    pub fn read_pin(&mut self, pin: Pin) -> Result<bool, I::Error> {
         let r = self.read_pins();
 
         if let Ok(data) = r {
--- src/pcf8575.rs
+++ src/pcf8575.rs
@@ -1,11 +1,11 @@
 //! Mod to use PCF8575
 use crate::{Pin, PinState};
-use embedded_hal::blocking::i2c::{self, Read};
+use embedded_hal::i2c::I2c;
 
 /// Struct to manage Pcf8575
 pub struct Pcf8575<'a, I>
 where
-    I: i2c::Write + i2c::Write,
+    I: I2c,
 {
     /// I2C driver
     i2c: &'a mut I,
@@ -17,7 +17,7 @@ where
 
 impl<'a, I> Pcf8575<'a, I>
 where
-    I: i2c::Write + i2c::Read,
+    I: I2c,
 {
     /// Create a new struct to manage Pcf8575
     pub fn new(i2c: &'a mut I, address: u8) -> Self {
@@ -29,21 +29,21 @@ where
     }
 
     /// Turn off all pins
-    pub fn clear(&mut self) -> Result<(), <I as i2c::Write>::Error> {
+    pub fn clear(&mut self) -> Result<(), I::Error> {
         self.i2c.write(self.address, &[0, 0])?;
         self.pins_state = 0;
         Ok(())
     }
 
     /// Turn on all pins
-    pub fn all_on(&mut self) -> Result<(), <I as i2c::Write>::Error> {
+    pub fn all_on(&mut self) -> Result<(), I::Error> {
         self.i2c.write(self.address, &[0xff, 0xff])?;
         self.pins_state = 0xffff;
         Ok(())
     }
 
     /// Send two bytes
-    pub fn write(&mut self, value: u16) -> Result<(), <I as i2c::Write>::Error> {
+    pub fn write(&mut self, value: u16) -> Result<(), I::Error> {
         let r = self.i2c.write(
             self.address,
             &[((value & 0xff00) >> 8) as u8, (value & 0xff) as u8],
@@ -57,7 +57,7 @@ where
     }
 
     /// Set up pins, don't change other
-    pub fn up_pins(&mut self, value: &[Pin]) -> Result<(), <I as i2c::Write>::Error> {
+    pub fn up_pins(&mut self, value: &[Pin]) -> Result<(), I::Error> {
         let mut new_value = self.pins_state;
 
         for pin in value {
@@ -68,7 +68,7 @@ where
     }
 
     /// Set up pins, don't change other
-    pub fn down_pins(&mut self, value: &[Pin]) -> Result<(), <I as i2c::Write>::Error> {
+    pub fn down_pins(&mut self, value: &[Pin]) -> Result<(), I::Error> {
         let mut new_value = self.pins_state;
 
         for pin in value {
@@ -79,7 +79,7 @@ where
     }
 
     /// Invert pins
-    pub fn toogle_pins(&mut self, value: &[Pin]) -> Result<(), <I as i2c::Write>::Error> {
+    pub fn toogle_pins(&mut self, value: &[Pin]) -> Result<(), I::Error> {
         let mut new_value = self.pins_state;
 
         for pin in value {
@@ -96,7 +96,7 @@ where
     }
 
     /// Set up don't change other
-    pub fn set_pins(&mut self, value: &[PinState]) -> Result<(), <I as i2c::Write>::Error> {
+    pub fn set_pins(&mut self, value: &[PinState]) -> Result<(), I::Error> {
         let mut new_value = self.pins_state;
 
         for state in value {
@@ -125,7 +125,7 @@ where
     }
 
     /// Read all pins status. Don't update internal state of this struct
-    pub fn read_pins(&mut self) -> Result<u16, <I as Read<u8>>::Error> {
+    pub fn read_pins(&mut self) -> Result<u16, I::Error> {
         let mut buffer: [u8; 2] = [0, 0];
 
         self.i2c.read(self.address, &mut buffer)?;
@@ -136,7 +136,7 @@ where
     }
 
     /// Read all pins status. Return true if pin is up
-    pub fn read_pin(&mut self, pin: Pin) -> Result<bool, <I as Read<u8>>::Error> {
+    pub fn read_pin(&mut self, pin: Pin) -> Result<bool, I::Error> {
         let r = self.read_pins();
 
         if let Ok(data) = r {
